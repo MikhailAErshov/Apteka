2b246963cda98705d4b6fb5bfaa8c9d39c475da1
MikhailAErshov <aem.cmd@gmail.com>
2023-06-29 15:03:20 +0300

    add branch-2

--- src/test/java/com/example/apteka/MainTest.java
+++ src/test/java/com/example/apteka/MainTest.java
@@ -33,8 +33,8 @@ public class MainTest extends WebTest{
 
     @BeforeEach
     public void setUp() {
-        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
-        open("/");
+//        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
+        open("https://aptekaeconom.com/");
         Selenide.webdriver().driver().getWebDriver().manage().addCookie(new Cookie("current_region", "103006"));
         refresh();
         aptekaMainPage.confirmRegion.shouldNotBe(visible);
--- src/test/java/com/example/apteka/WebTest.java
+++ src/test/java/com/example/apteka/WebTest.java
@@ -1,9 +1,6 @@
 package com.example.apteka;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -17,13 +14,6 @@ import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 public class WebTest {
     @BeforeAll
     public static void setDriver() throws MalformedURLException {
-        WebDriverManager.chromedriver().setup();
-        Configuration.browser = "chrome";
-        Configuration.baseUrl = "https://aptekaeconom.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "none";
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
         boolean isRemote = true;
         if (isRemote) {
             DesiredCapabilities capabilities = new DesiredCapabilities();
